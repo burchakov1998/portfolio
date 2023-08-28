55c445a466732065cb0478308c1501908688af83
burchakov1998 <124458140+burchakov1998@users.noreply.github.com>
2023-08-28 15:00:35 +0300

    modifed

---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import './Portfolio.css';
+import ProjectList from '../ProjectList/ProjectList';
+import Toolbar from '../Toolbar/Toolbar';
+
+function Portfolio({ projectList }) {
+  const filterButtonList = ['All', 'Websites', 'Flayers', 'Business Cards'];
+
+  const [filteredListProjects, setFilteredListProjects] = useState(projectList);
+  const [selected, setSelected] = useState('All');
+
+  const filter = (e) => {
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((item) => item.classList.remove('active'));
+    
+    e.target.classList.add('active');
+
+    const select = e.target.textContent;
+    
+    if (select === 'All') {
+      setFilteredListProjects(projectList);
+    } else {
+      setFilteredListProjects(
+        projectList.filter(item => item.category === select)
+      );
+    }
+
+    setSelected(select);
+
+    console.log(select);
+  }
+
+  return ()
+}
+
+export default Portfolio;
